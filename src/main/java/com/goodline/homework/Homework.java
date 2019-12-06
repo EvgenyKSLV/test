2e84994d8a4d92757c9a5b566a6e87e199c7d639
Zhenya <Zhenya310596@mail.ru>
2019-12-06 16:42:04 +0700

    Первая домашняя работа Райфекершт

--- src/main/java/com/goodline/homework/Homework.java
+++ src/main/java/com/goodline/homework/Homework.java
@@ -15,6 +15,7 @@ public class Homework {
     }
 
     public static int[] printEven() {
+        // Сделано
         // Вернуть массив четных чисел меньше 100
         int[] numarr = new int[50];
         for (int i = 2 , j = 0 ; i < 100 ; i += 2 , j ++ )
@@ -25,7 +26,8 @@ public class Homework {
     }
 
     private static void printNames() {
-       // Вывести все элементы массива names
+       // Сделано
+        // Вывести все элементы массива names
        for (int i = 0 ; i <= numbers.length ; i ++) 
        {
            System.out.println(numbers[i]);
@@ -33,6 +35,7 @@ public class Homework {
     }
 
     private static int getCount() {
+        // Сделано
         // Вернуть количество цифр, которые делятся на 3 и 7 без остатка в массиве numbers
         int[] numbers1 = new int[numbers.length];
         for (int i = 0 ; i  <= numbers.length ; i ++) 
@@ -46,6 +49,7 @@ public class Homework {
     }
 
     private static void printCounts() {
+        // Сделано
         // СЛОЖНОЕ ЗАДАНИЕ
         // Вывести количество всех цифр в массиве numbers
         // Допустим [1, 1, 1, 2, 2, 3, 3, 3, 4]
